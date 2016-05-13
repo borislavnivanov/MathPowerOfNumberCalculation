26c1ba61807cbed5ffa6ca4c197883dead30eca7
borislavnivanov <borislavnivanov@gmail.com>
2016-05-13 20:55:45 +0300

    Add project files.

---
+++ MathPowerOfNumberCalculation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MathPowerOfNumberCalculation
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is the base number?");
+            int b;
+            string baseTextValue = Console.ReadLine();
+            b = int.Parse(baseTextValue);
+
+            Console.WriteLine("To the power of?");
+            int p;
+            string powerTextValue = Console.ReadLine();
+            p = int.Parse(powerTextValue);
+
+            Console.WriteLine(Math.Pow(b, p));
+
+            Console.ReadLine();
+        }
+    }
+}
